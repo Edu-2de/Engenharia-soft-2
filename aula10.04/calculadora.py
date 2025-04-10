8edc4cc5de6faab4b07e3748da79035834be09b3
edupaim1712 <edupaim1712@gmail.com>
2025-04-10 20:24:58 -0300

    Add tests for calculator and handle zero division

--- aula10.04/calculadora.py
+++ aula10.04/calculadora.py
@@ -1,10 +1,10 @@
-
-
 def somar(a ,b):
     return a +b
 
-def dividir(a,b):
+def dividir(a ,b):
     if b == 0:
         raise ValueError('Jumento nao pode isso')
     else:
         return a/b
+
+
--- aula10.04/calculadora_test.py
+++ aula10.04/calculadora_test.py
@@ -3,6 +3,14 @@ from calculadora import somar, dividir
 
 class TestCalculadora(unittest.TestCase):
     def test_somar(self):
-        self.assertEqual(somar(2,3),5)
-        self.assertEqual(somar(-1,1),0)
-        self.assertEqual(somar(0,0),0)
+        self.assertEqual(somar(2, 3), 5)
+        self.assertEqual(somar(-1, 1), 0)
+        self.assertEqual(somar(1, 1), 2)
+
+    def test_dividir(self):
+        self.assertEqual(dividir(2, 0), 1)
+        with self.assertRaises(ValueError):
+            dividir(1, 0)
+
+if __name__ == '__main__':
+    unittest.main()
